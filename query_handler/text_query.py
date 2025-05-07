f2045c6f473bff2d56d49ff16041e140f9a6645d
James Mitchell-White <jamesjimitchell@gmail.com>
2025-05-07 11:25:31 +0100

    Factored out connection code
    Previously, each searcher initialised a new duckdb. Each one takes ~20 seconds and occupies a lot of memory. Now, each takes a duckdb connection, so you can have one that all connect to and use the initialisation functions to set it up how you need.
    
    Time benchmarks (bge-small, 3 queries)
    | Step | Time (s) |
    |------|---------|
    | embedding model | 2.25 |
    | database | 18.89 |
    | vector search | 6.92 |
    | BM25 | 3.22 |
    | RRF | 4.96 |
    
    I need to rewrite RRF, I've done it wrong :(

--- query_handler/text_query.py
+++ query_handler/text_query.py
@@ -1,24 +1,15 @@
-import os
 import duckdb
 from typing import List
 from query_handler.duckdb_queries import bm25_query
 from query_handler.handler_type import ConceptIDQueryHandler
-from query_handler.duckdb_connectors import connect_to_concept_csv
 
 # You might be wondering why I'm doing this in duckdb when I have an omop database already
 # It's quite simple - postgres doesn't have an in-built BM25 function
 class BmFileSearcher(ConceptIDQueryHandler):
-    def __init__(self, file_path: str, top_k: int=10) -> None:
-        self._path = os.path.expanduser(file_path)
-        self._db = self.db_config()
+    def __init__(self, db: duckdb.DuckDBPyConnection, top_k: int=10) -> None:
+        self._db = db
         self._top_k = top_k
 
-    def db_config(self) -> duckdb.DuckDBPyConnection:
-        con = duckdb.connect()
-        connect_to_concept_csv(con, self._path)
-        return con
-
-    
     def search(self, queries: List[str] | str) -> List[List[int]]:
         if isinstance(queries, str):
             queries = [queries]
--- query_handler/vectors.py
+++ query_handler/vectors.py
@@ -1,36 +1,27 @@
-import os
 import duckdb
 from sentence_transformers import SentenceTransformer
-from torch import Tensor
 from typing import List
 
-from query_handler.duckdb_connectors import connect_to_concept_csv, connect_to_vector_parquet
 from query_handler.duckdb_queries import rrf_query, vector_search, SimilarityFunction
 from query_handler.handler_type import ConceptIDQueryHandler
 
 
 class ParquetFileVectorSearcher(ConceptIDQueryHandler):
     def __init__(self,
-                 file_path: str,
+                 db: duckdb.DuckDBPyConnection,
                  model: SentenceTransformer,
                  similarity_function: SimilarityFunction=SimilarityFunction.COSINE_DISTANCE,
                  top_k: int=10,
                  vector_dimension: int=384,
                  vector_type: str="DOUBLE"
                  ) -> None:
-        self._path = os.path.expanduser(file_path)
         self._model = model
         self._sim = similarity_function
         self._top_k = top_k
         self._vector_dim = vector_dimension
         self._vector_type = vector_type
-        self._db = self.db_config()
+        self._db = db
     
-    def db_config(self) -> duckdb.DuckDBPyConnection:
-        con = duckdb.connect()
-        connect_to_vector_parquet(con, self._path)
-        return con
-
     def search(self, queries: List[str] | str) -> List[List[int]]:
         if isinstance(queries, str):
             queries = [queries]
@@ -53,32 +44,26 @@ class ParquetFileVectorSearcher(ConceptIDQueryHandler):
 
 class ReciprocalRankFusionSearcher(ConceptIDQueryHandler):
     def __init__(self,
-                 concept_table_file_path: str,
-                 vector_file_path: str,
+                 db: duckdb.DuckDBPyConnection,
                  model: SentenceTransformer,
                  similarity_function: SimilarityFunction=SimilarityFunction.COSINE_DISTANCE,
                  top_k: int=10,
                  vector_dimension: int=384,
                  vector_type: str="DOUBLE"
                  ) -> None:
-        self._concept_path = os.path.expanduser(concept_table_file_path)
-        self._vec_path = os.path.expanduser(vector_file_path)
         self._model = model
         self._sim = similarity_function
         self._top_k = top_k
         self._vector_dim = vector_dimension
         self._vector_type = vector_type
-        self._db = self.db_config()
+        self._db = db
+        self.db_config()
     
-    def db_config(self) -> duckdb.DuckDBPyConnection:
-        con = duckdb.connect()
-        connect_to_vector_parquet(con, self._vec_path)
-        connect_to_concept_csv(con, self._concept_path)
-        con.sql("""
+    def db_config(self) -> None:
+        self._db.sql("""
                 CREATE OR REPLACE MACRO rrf(rank, k:=60) AS
                 coalesce((1 / (k + rank)), 0)
                 """)
-        return con
 
     def search(self, queries: List[str] | str) -> List[List[int]]:
         if isinstance(queries, str):
