78a5a41bfd5348c0116c45e3829815aa73373b29
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-06-09 08:03:55 +0100

    Completed test for the custom metrics integration with mlflow.evaluate - debugged input and output format issue

--- integrations/mlflow/custom_metrics.py
+++ integrations/mlflow/custom_metrics.py
@@ -6,15 +6,11 @@ from mlflow.metrics import make_metric, MetricValue
 from evaluation.metrics import ExactMatch 
 
 
-def exact_match_eval_fn(
-    eval_df, 
-    prediction_col: str = "predictions", 
-    target_col: str = "target"
-) -> MetricValue:
+def exact_match_eval_fn(predictions: pd.Series,  targets: pd.Series) -> MetricValue:
     exact_match = ExactMatch()
     scores = [
         exact_match.calculate(pred, target)
-        for pred, target in zip(eval_df[prediction_col], eval_df[target_col])
+        for pred, target in zip(predictions, targets)
     ]
 
     return MetricValue(
@@ -31,9 +27,3 @@ exact_match_metric = make_metric(
     eval_fn=exact_match_eval_fn, 
     greater_is_better=True 
 )
-
-
-
-
-
-
