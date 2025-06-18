ac6abaf5d69b0b760509a80ccc490235f51fb0f3
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-06-18 11:12:09 +0100

    Removed old-dated references to pytest fixture llama models

--- tests/test_pipeline_wrapper.py
+++ tests/test_pipeline_wrapper.py
@@ -4,12 +4,13 @@ import mlflow
 from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
 
-from integrations.mlflow.pipeline_wrapper import MLflowPipelineWrapper, MLflowRAGPipeline
+from integrations.mlflow.pipeline_wrapper import MLflowLLMPipeline, MLflowRAGPipeline
 from integrations.mlflow.config import (
     LLMConfig, 
     EmbeddingConfig, 
     RetrievalConfig, 
     DatabaseConfig, 
+    LLMPipelineConfig, 
     RAGPipelineConfig
 )
 from evaluation.pipelines import LLMPipeline
@@ -17,47 +18,31 @@ from components.models import local_models, LLMModel
 from components.embeddings import get_embedding_model 
 
 
-@pytest.fixture(scope="session") 
-def llama_model(
-    model_name: str = LLMModel.TINYLLAMA_1_1B_CHAT.value, 
-    n_ctx: int = 512, 
-    n_batch: int = 32, 
-    max_tokens: int = 128
-): 
-    return Llama(
-        hf_hub_download(**local_models[model_name]),
-        model_kwargs={
-            "n_ctx": n_ctx,
-            "n_batch": n_batch,
-            "n_gpu_layers": -1,
-            "verbose": True
-        },
-        generation_kwargs={"max_tokens": max_tokens, "temperature": 0.}
-    )
-
-
 @pytest.fixture(scope="session")
-def prompt_template_str(): 
-    return """You will be given the informal name of a medication. Respond only with the formal name of that medication, without any extra explanation.
+def prompt_template_str_simple(): 
+    return """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+    
+You will be given the informal name of a medication. Respond only with the formal name of that medication, without any extra explanation.
 
-    Examples:
+Examples:
 
-    Informal name: Tylenol
-    Response: Acetaminophen
+Informal name: Tylenol
+Response: Acetaminophen
 
-    Informal name: Advil
-    Response: Ibuprofen
+Informal name: Advil
+Response: Ibuprofen
 
-    Informal name: Motrin
-    Response: Ibuprofen
+Informal name: Motrin
+Response: Ibuprofen
 
-    Informal name: Aleve
-    Response: Naproxen
+Informal name: Aleve
+Response: Naproxen
 
-    Task:
+Task:
 
-    Informal name: {{informal_name}}<|eot_id|>
-    Response: """   
+Informal name: {{informal_name}}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+"""   
 
 
 @pytest.fixture(scope="session")
@@ -92,18 +77,18 @@ Informal name: {{informal_name}}<|eot_id|><|start_header_id|>assistant<|end_head
 
 """
 
-def test_predict_llm_pipeline_wrapper(llama_model, prompt_template_str):     
-    pipeline = LLMPipeline(
-        llama_model, 
-        prompt_template_str=prompt_template_str, 
-        template_vars=["informal_name"]
-    )
-    wrapper = MLflowPipelineWrapper(pipeline, pipeline_type="llm")
+def test_predict_llm_pipeline_wrapper(prompt_template_str_simple):     
     model_input = pd.DataFrame({
         "input_data": ["paracetamol", "codeine"], 
         "expected_output": ["acetaminophen", "codeine"]
     })
-    predictions = wrapper.predict(model_input=model_input)
+    config = LLMPipelineConfig(
+        llm=LLMConfig(model_name=LLMModel.LLAMA_3_1_8B.value),
+        prompt_template=prompt_template_str_simple, 
+        template_vars = ["informal_name", "vec_results"]
+    )
+    pipeline = MLflowLLMPipeline(config)
+    predictions = pipeline.predict(model_input=model_input)
     assert predictions["predictions"].iloc[0].strip().lower() == "acetaminophen"
     assert predictions["predictions"].iloc[1].strip().lower() == "codeine"
 
@@ -127,26 +112,22 @@ def test_predict_rag_pipeline_wrapper(prompt_template_str_rag):
     assert predictions["predictions"].iloc[1].strip().lower() == "codeine"
 
 
-def test_error_thrown_if_input_data_not_present(): 
-    pass 
-
-
-def test_pyfunc_model_logging(tmp_path, llama_model, prompt_template_str): 
+def test_pyfunc_model_logging(tmp_path): 
     tracking_uri = tmp_path.as_uri()
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment("pyfunc_llm_test")
 
     with mlflow.start_run() as run:
         # Log the model to the run's artifacts
-        pipeline = LLMPipeline(
-            llama_model, 
-            prompt_template_str=prompt_template_str, 
-            template_vars=["informal_name"]
+        config = LLMPipelineConfig(
+            llm=LLMConfig(model_name=LLMModel.LLAMA_3_1_8B.value),
+            prompt_template=prompt_template_str_simple, 
+            template_vars = ["informal_name", "vec_results"]
         )
-        wrapper = MLflowPipelineWrapper(pipeline, pipeline_type="llm")
+        pipeline = MLflowLLMPipeline(config)
         mlflow.pyfunc.log_model(
             artifact_path="llm_model",
-            python_model=wrapper
+            python_model=pipeline
         )
         run_id = run.info.run_id 
 
